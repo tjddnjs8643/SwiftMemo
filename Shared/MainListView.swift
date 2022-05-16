162225172cdde6bd51ff570efcbe1804438d576b
tjddnjs8643 <tjddnjs7543@gmail.com>
2022-05-16 14:17:23 +0900

    #main [feat] swiftMemo 생성

---
+++ Shared/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 이성원 on 2022/05/16.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
