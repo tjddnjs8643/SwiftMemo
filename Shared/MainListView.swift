f23136376a718362b9150db97dc3bf44d5a73ef0
tjddnjs8643 <tjddnjs7543@gmail.com>
2022-05-16 14:48:10 +0900

    step3

--- Shared/MainListView.swift
+++ Shared/MainListView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store: MemoStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list){ memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .foregroundColor(.brown)
+                        .lineLimit(1)
+                    
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.mint)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
+            
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())
     }
 }
